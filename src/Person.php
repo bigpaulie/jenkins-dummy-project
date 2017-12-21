d8c4ce4c7978a29dbe85029c4b6301e1a4df2629
Paul P <bigpaulie25ro@yahoo.com>
2017-12-21 22:33:12 +0200

    Adding person

---
+++ src/Person.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace bigpaulie\jenkins;
+
+/**
+ * Class Person
+ * @package bigpaulie\jenkins
+ */
+class Person
+{
+
+    /**
+     * Person speaks French.
+     *
+     * @var string
+     */
+    const LANGUAGE_FRENCH = 'french';
+
+    /**
+     * Person speaks English.
+     *
+     * @var string
+     */
+    const LANGUAGE_ENGLISH = 'english';
+
+    /**
+     * @var Talk $talk
+     */
+    private $talk;
+
+    /**
+     * Person constructor.
+     * @param Talk $talk
+     */
+    public function __construct(Talk $talk = null)
+    {
+        $this->talk = $talk;
+    }
+
+    public function greet($name, $language = self::LANGUAGE_ENGLISH)
+    {
+        if ( $this->talk ) {
+            return $this->talk->sayHello($name);
+        }
+
+        return null;
+    }
+}
